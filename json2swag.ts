9f097e1b20909c7ef95948aafeed87a23ca9794f
kitada <k.kitada@fb.tapweb.jp>
2022-12-08 18:38:01 +0900

    up

--- json2swag.ts
+++ json2swag.ts
@@ -4,18 +4,18 @@ const head = String.raw`
    /**
     * @return array
     * @OA\Get(
-    *      path="xxx",
-    *      summary="SUMMARY",
-    *      description="DESCRIPTION",
-    *      @OA\Response(
-    *          response="200",
-    *          description="成功時",
-    *          @OA\JsonContent(
-    *              type="object",`;
+    *     path="xxx",
+    *     summary="SUMMARY",
+    *     description="DESCRIPTION",
+    *     @OA\Response(
+    *         response="200",
+    *         description="成功時",
+    *         @OA\JsonContent(
+    *             type="object",`;
 
 const foot = String.raw`
-    *          ),
-    *      ),
+    *         ),
+    *     ),
     * );
     */
 `;
@@ -23,16 +23,38 @@ const foot = String.raw`
 function main(): void {
     const input = fs.readFileSync("/dev/stdin", "utf8");
     const obj = JSON.parse(input);
-    let res : string = head;
+    let contents: string = head;
+    let indent = 3;    // 深さ
 
-    for(const key in obj){
+    for (const key in obj) {
+        const property = key;
+        const type = 'string';
+
+        contents += "\n";
+        contents += addSpace(String.raw`@OA\Property(`, indent);
+        contents += "\n";
+        contents += addSpace(`property="${property}",`, indent + 1);
+        contents += "\n";
+        contents += addSpace(`type="${type}",`, indent + 1);
+        contents += "\n";
+        contents += addSpace('),', indent);
 
     }
 
-    res += foot;
+    contents += foot;
 
 
-    console.log(res);
+    console.log(contents);
+}
+
+function addSpace(str: string, indent: number): string {
+    let s = '';
+    for (let i = 0; i < indent; i++) {
+        s += '    ';
+    }
+    const space = '    * ' + s;
+
+    return space + str;
 }
 
 
--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 function test_func() {
     input=$1
     expected=$2
+    no=$3
 
     output=$(echo "$input" | node json2swag.js)
     if [ $output != $expected ]; then
-        echo "NG"
-        echo "expected: $expected"
-        echo "output: $output"
+        echo "NG $no expected vs output"
+        #echo "expected: $expected"
+        #echo "output: $output"
+        diff <(echo "$expected") <(echo "$output")
         exit 1
     fi
 }
@@ -15,27 +17,57 @@ function test_func() {
 tsc json2swag.ts
 
 # 1
+no=1
 input='{}'
-expected=`cat << DOC
+expected=$(
+    cat <<DOC
 
    /**
     * @return array
     * @OA\Get(
-    *      path="xxx",
-    *      summary="SUMMARY",
-    *      description="DESCRIPTION",
-    *      @OA\Response(
-    *          response="200",
-    *          description="成功時",
-    *          @OA\JsonContent(
-    *              type="object",
-    *          ),
-    *      ),
+    *     path="xxx",
+    *     summary="SUMMARY",
+    *     description="DESCRIPTION",
+    *     @OA\Response(
+    *         response="200",
+    *         description="成功時",
+    *         @OA\JsonContent(
+    *             type="object",
+    *         ),
+    *     ),
     * );
     */
 DOC
-`
+)
+test_func $input $expected $no
 
-test_func $input $expected
+# 2
+no=2
+input='{"key": "value"}'
+expected=$(
+    cat <<DOC
+
+   /**
+    * @return array
+    * @OA\Get(
+    *     path="xxx",
+    *     summary="SUMMARY",
+    *     description="DESCRIPTION",
+    *     @OA\Response(
+    *         response="200",
+    *         description="成功時",
+    *         @OA\JsonContent(
+    *             type="object",
+    *             @OA\Property(
+    *                 property="key",
+    *                 type="string",
+    *             ),
+    *         ),
+    *     ),
+    * );
+    */
+DOC
+)
+test_func $input $expected $no
 
 echo "OK"
